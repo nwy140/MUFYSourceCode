dd0568141e45716e8bfa3cacb5713b761b4b4a8f
NG WEI YUEN <17109182@SUNWAYEDU.LOCAL>
2018-09-07 09:44:40 +0800

    Added script kiddies reference 2

--- vb.net/HelloScriptKiddies/ConsoleApplication1/Module1.vb
+++ vb.net/HelloScriptKiddies/ConsoleApplication1/Module1.vb
@@ -2,6 +2,17 @@
 
     Sub Main()
 
+        'ScriptEngine Kiddie input your name
+        Dim YourName As String
+        Dim StatusDate As String
+        StatusDate = InputBox("Your are a script kiddie for viewing this, input your name")
+        If StatusDate = " " Then
+            MessageBox.Show("Null")
+
+        ElseIf StatusDate = "" Then
+
+        End If
+
         'declare script kiddie text as string
         Dim FirstName As String = " Script"
         Dim LastName As String = " Kiddie"
@@ -9,9 +20,12 @@
 
         For i As Integer = 0 To 100000000
             'print script kiddie text
-            Console.WriteLine("Hello" + FirstName + LastName + Str(100 / 2) + " Iterating: " + Str(i)
-                              )
+            Console.WriteLine("Hello " + StatusDate + " is a " + FirstName + LastName + ": " + Str(100 / 2) + " Iterating: " + Str(i))
         Next
     End Sub
+
+    Private Function MessageBox() As Object
+        Throw New NotImplementedException
+    End Function
 
 End Module
