322a8e441b96f35ec8cc1589936b09b42df88af3
nwy140 <nwy140@gmail.com>
2018-09-12 08:10:33 +0800

    added script kiddie excercises

---
+++ python/script Kiddie exercises/1_Intro.py
@@ -0,0 +1,30 @@
+
+print ("hello world")
+
+
+LastName="Potter"
+
+FirstName = "Harry"
+
+print (FirstName +' '+ LastName)
+
+print (90)
+
+print (90/2)
+
+print (100*2)
+
+A = 9
+
+print (A)
+
+
+Name =' no name'
+Name = input ('whats your name')
+print ('Hello ' +str(Name))
+
+Age =input ('what is your age')
+Next = int(Age)+1
+print ('on your next birthday, you will be ' + str(Next))
+
+
