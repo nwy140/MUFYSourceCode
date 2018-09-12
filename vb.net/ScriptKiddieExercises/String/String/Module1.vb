c2f71f1705287b70194fa51e8fab55c962a9889f
NG WEI YUEN <17109182@SUNWAYEDU.LOCAL>
2018-09-12 09:31:09 +0800

    Added vb.net string

---
+++ vb.net/ScriptKiddieExercises/String/String/Module1.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Sub Main()
+        'ScriptEngine Kiddie input your name
+
+
+        'Ex 1
+        Dim YourName As String
+
+        YourName = InputBox("Your are a script kiddie for viewing this, input your name ")
+        Console.WriteLine("your name is : " + YourName)
+
+        'Ex 2
+        Console.WriteLine("Dexter was named after Dexter from the tv show 'Perfect Match'")
+
+        'Ex 3
+        Dim Town As String
+        Town = InputBox("What is your town name?")
+        Console.WriteLine(Town.ToLower())
+
+        'Ex 4
+        Town = "Jakarta"
+        Console.WriteLine(Town.Contains("k"))
+
+        'Ex 5
+        Dim River As String
+
+        River = "Mississippi"
+        Console.WriteLine(River)
+        River = River.Replace("s", "0")
+        Console.WriteLine(River)
+
+        'Ex 6
+        Dim Password As String
+        Password = InputBox("Enter Password")
+        Console.WriteLine("Length of password : " + Str(Password.Length()))
+
+
+
+
+    End Sub
+
+
+End Module
