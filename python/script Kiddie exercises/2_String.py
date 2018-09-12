9a838dc7eaf523ae4504e856fcaa0fe63a82a5bd
nwy140 <nwy140@gmail.com>
2018-09-12 08:19:06 +0800

    Added string.py

---
+++ python/script Kiddie exercises/2_String.py
@@ -0,0 +1,13 @@
+
+
+print ('Dexter was named after Dexter from the television show “Perfect Match')
+
+Uppercase = 'MUFY IS FOR SCRIPT KIDDIES'
+
+lowercase = Uppercase.lower()
+print (lowercase)
+
+check = 'abcd'
+
+
+	
