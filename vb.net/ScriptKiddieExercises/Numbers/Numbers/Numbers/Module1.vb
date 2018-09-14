17972e94f27f03acd87e219ef4807ae4d7312f74
NG WEI YUEN <17109182@SUNWAYEDU.LOCAL>
2018-09-14 09:04:55 +0800

    Added Number vb.net

---
+++ vb.net/ScriptKiddieExercises/Numbers/Numbers/Numbers/Module1.vb
@@ -0,0 +1,56 @@
+﻿Module Module1
+
+    Sub Main()
+        'Ex1
+        Console.WriteLine((4 + 5).ToString())
+        Console.WriteLine((3 - 4).ToString())
+        Console.WriteLine((45 / 9).ToString())
+        Console.WriteLine((3 * 6).ToString())
+
+        'Ex2
+        Console.WriteLine((3 + 4 * 2).ToString())
+        Console.WriteLine((40 / 8 - 2).ToString())
+        Console.WriteLine((7 - 5 * 2).ToString())
+        Console.WriteLine((7 * 7 + 7).ToString())
+
+        'Ex3
+        Console.WriteLine(((3 + 4) * 2).ToString())
+        Console.WriteLine((45 - (8 - 2)).ToString())
+        Console.WriteLine((7 - (5 * 2)).ToString())
+        Console.WriteLine((7 * 7 + 7).ToString())
+
+        'Ex4
+        Dim Num1 As Integer = CInt(InputBox("Enter Number 1"))
+
+
+        Dim Num2 As Integer = CInt(InputBox("Enter Number 2"))
+
+        Dim Num3 As Integer = CInt(InputBox("Enter Number 3"))
+
+        Dim Total As Integer = Num1 + Num2 + Num3
+
+        Dim Average As Integer = Total / 3
+        Console.WriteLine("Total: " + (Total).ToString() + " Average: " + (Average).ToString())
+
+        'Ex5
+        Dim daysHired As Integer = CInt(InputBox("Enter Number of Days a game is hired"))
+        Dim output As Double = 5.5 + 4.5 * daysHired
+        Console.WriteLine("Days Hired: " + (daysHired).ToString() + " Output: " + (output).ToString())
+
+        'Ex6
+        Console.WriteLine((2 ^ 2).ToString())
+        Console.WriteLine((3 ^ 2).ToString())
+        Console.WriteLine((4 ^ 2).ToString())
+        Console.WriteLine((5 ^ 2).ToString())
+
+        Console.WriteLine("Alternate way using for loop")
+        'Alternate way
+
+        For index As Integer = 1 To 5
+            Console.WriteLine((index) ^ 2.ToString)
+        Next
+
+
+    End Sub
+
+End Module
