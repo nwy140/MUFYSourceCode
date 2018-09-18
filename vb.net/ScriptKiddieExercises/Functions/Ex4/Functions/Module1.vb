17f98c8ea9e6df261b0c9ea837c1ffd2789e2702
nwy140 <nwy140@gmail.com>
2018-09-19 04:22:49 +0800

    Update vn.bet functions

---
+++ vb.net/ScriptKiddieExercises/Functions/Ex4/Functions/Module1.vb
@@ -0,0 +1,37 @@
+﻿Module Module1
+    'Declare global variables
+    Dim Days As Integer
+    Dim Cost As Single
+    Const charge As Single = 10
+    Const rental As Single = 66
+
+    Sub Main()
+
+        Call Input()
+        ' Set days as input's return value
+
+        'proocess days
+        Call Processing(Days)
+
+        'Call output to display
+        Call Output()
+    End Sub
+
+    Sub Input()
+        Console.WriteLine("How many days do you want the hire ski? ")
+
+        Days = Console.ReadLine
+    End Sub
+
+    Function Processing(Days As String )
+   ' Calculate rental by day and add charges 
+        Cost = (Days* rental) + charge
+    End Function
+
+    Sub Output()
+        Console.WriteLine("The cost of the ski hire is" & FormatCurrency(Cost))
+    End Sub
+
+
+
+End Module
---
+++ vb.net/ScriptKiddieExercises/Functions/Ex5/Functions/Module1.vb
@@ -0,0 +1,46 @@
+﻿Module Module1
+    'Declare global variables
+    Dim Hours As Integer
+    Dim Cost As Single
+    Const charge As Single = 10
+    Const rental As Single = 4.50
+    Dim bikeNum As Integer = 0
+    Dim name As String =""
+
+    Sub Main()
+
+        Call Input()
+        ' Set hours as input's return value
+
+        'proocess hours
+        Call Processing(Hours, bikeNum)
+
+        'Call output to display
+        Call Output()
+    End Sub
+
+    Sub Input()
+        Console.WriteLine("Enter Name ")
+        
+        name = Console.ReadLine
+        Console.WriteLine("How many bicycles do you want to hire? ")
+
+        bikeNum = Cint(Console.ReadLine) 
+        Console.WriteLine("How many hours do you want the hire bicycle? ")
+
+        Hours = Console.ReadLine
+    End Sub
+
+    Function Processing(Hours As String , bikeNum)
+   ' Calculate rental by hour and add charges 
+        Cost = bikeNum * Hours* rental + charge
+    End Function
+
+    Sub Output()
+       ' Display name,  bicycle number and total cost
+        Console.WriteLine("""name"""& ", the cost of " & bikeNum & " bicycles for 2 hours is a TOTAL of " & FormatCurrency(Cost))
+    End Sub
+
+
+
+End Module
