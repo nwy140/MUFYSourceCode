391e6adc2989b2f8b253e2ecd13c4d76a143c658
Nwy <nwy140@gmail.com>
2018-09-20 15:34:03 +0800

    Quick Com

--- vb.net/ScriptKiddieExercises/Repetition/Excercise 6/Excercise 7/Module1.vb
+++ vb.net/ScriptKiddieExercises/Repetition/Excercise 6/Excercise 7/Module1.vb
@@ -2,27 +2,38 @@
 
     Dim userName As String
     Dim password As String
+    Dim authMessage As String = "Application Locked"
 
     Sub Main()
+        Console.WriteLine("Authenticating Vault")
 
+        '3 Changes of entering authentication
+        For i = 1 To 3
+            'User enters authentication
+            Console.WriteLine("Please Enter Username")
+            userName = Console.ReadLine()
 
+            Console.WriteLine("Please Enter Password")
+            password = Console.ReadLine()
 
-        Console.WriteLine("Please Enter Username")
-        userName = Console.ReadLine()
+            'If statement ensures user enters correct username and password
+            If userName <> "dedsec" And password <> "retr0" Then
+                Console.WriteLine("Incorrect Username & Password")
+            ElseIf userName <> "dedsec" Then
+                Console.WriteLine("Incorrect Username")
+            ElseIf password <> "retr0" Then
+                Console.WriteLine("Incorrect Password ")
+            Else
+                'Change authentication message
+                authMessage = "Vaults Opens"
+                'Continue / Break equilavent in vb.net from C++
+                Exit For
+            End If
+        Next
+        Console.WriteLine(authMessage)
 
-
-        Console.WriteLine("Please Enter Password")
-        password = Console.ReadLine()
-
-        If userName = Not "dedsec" And password = Not "retr0" Then
-            Console.WriteLine("Incorrect Username & Password")
-        ElseIf userName = Not "dedsec" Then
-            Console.WriteLine("Incorrect Username")
-        ElseIf password = Not "retro0" Then
-            Console.WriteLine("Incorrect Password")
-        Else
-            Console.WriteLine("Vault Opens")
-        End If
+        'Stop program from exiting on release build
+        Console.ReadLine()
     End Sub
 
 End Module
