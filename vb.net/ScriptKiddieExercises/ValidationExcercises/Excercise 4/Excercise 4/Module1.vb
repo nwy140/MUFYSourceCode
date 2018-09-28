80066a72599f85d8145ea1428558dcde459005bc
NG WEI YUEN <17109182@SUNWAYEDU.LOCAL>
2018-09-28 09:29:20 +0800

    Improved vb...2

--- vb.net/ScriptKiddieExercises/ValidationExcercises/Excercise 4/Excercise 4/Module1.vb
+++ vb.net/ScriptKiddieExercises/ValidationExcercises/Excercise 4/Excercise 4/Module1.vb
@@ -2,7 +2,7 @@
 
     'Declare global variables
     Dim Weight As String
-    Dim Height As String = 0
+    Dim Height As String
     Dim BodyMassIndex As Single = 0
 
     Sub Main()
@@ -17,9 +17,7 @@
             Weight = Console.ReadLine()
             If Weight = "" Then
                 Console.WriteLine("Weight not entered properly - please try again")
-                Exit While
                 Weight = ""
-
             End If
             If IsNumeric(Weight) = False Or Weight > 200 Or Weight < 0 Then
                 Console.WriteLine("Weight must be entered as a number, Weight must be between 0 and 200 KG")
@@ -47,8 +45,64 @@
 
     Sub CalculateBMI()
         BodyMassIndex = Weight / (Height ^ 2)
+
+        Console.WriteLine("BMI : " & BodyMassIndex)
     End Sub
 
 
 End Module
 
+
+'Module Module1
+
+'    'Declare global variables
+'    Dim Weight As String
+'    Dim Height As String = 0
+'    Dim BodyMassIndex As Single = 0
+
+'    Sub Main()
+'        Call InputInfo()
+'    End Sub
+
+'    Sub InputInfo()
+
+'        'User inputs Weight , Weight must be present and is number and is between 200 and 0
+'        While Weight = "" Or IsNumeric(Weight) = False Or Weight > 200 Or Weight < 0
+'            Console.WriteLine("enter your Weight")
+'            Weight = Console.ReadLine()
+'            If Weight = "" Then
+'                Console.WriteLine("Weight not entered properly - please try again")
+'                Exit While
+'                Weight = ""
+
+'            End If
+'            If IsNumeric(Weight) = False Or Weight > 200 Or Weight < 0 Then
+'                Console.WriteLine("Weight must be entered as a number, Weight must be between 0 and 200 KG")
+'            End If
+
+'        End While
+
+'        'User inputs Height , Height must be present and is number and is between 200 and 0
+'        While Height = "" Or IsNumeric(Height) = False Or Height > 200 Or Height < 0
+'            Console.WriteLine("enter your Height")
+'            Height = Console.ReadLine()
+'            If Height = "" Then
+'                Console.WriteLine("Height not entered properly - please try again")
+'            End If
+'            If IsNumeric(Height) = False Or Height > 200 Or Height < 0 Then
+'                Console.WriteLine("Height must be entered as a number, Height must be between 0 and 200 KG")
+'            End If
+
+'        End While
+
+
+'        'Display valid BMI text when user enters information correctly
+'        Call CalculateBMI()
+'    End Sub
+
+'    Sub CalculateBMI()
+'        BodyMassIndex = Weight / (Height ^ 2)
+'    End Sub
+
+
+'End Module
