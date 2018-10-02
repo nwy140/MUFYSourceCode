97697b07711aab03cd3efe0900a7ef2d18904ef2
NG WEI YUEN <17109182@SUNWAYEDU.LOCAL>
2018-10-02 16:27:00 +0800

    quick com

--- vb.net/ScriptKiddieExercises/PseudocodeToApplication/Excercise 5/Excercise 5/Module1.vb
+++ vb.net/ScriptKiddieExercises/PseudocodeToApplication/Excercise 5/Excercise 5/Module1.vb
@@ -11,10 +11,25 @@
 
     Sub inputpass()
         Console.WriteLine("Select 7 , 14 or 30 Day Pass, Enter Exit to exit")
+        daypass = Console.ReadLine
 
     End Sub
 
     Sub evaluate()
         While daypass <> "exit"
+
+            If daypass = "exit" Then
+                Exit While
+      
+            ElseIf daypass = 7 Then
+                Console.WriteLine("7 Day Pass cost $136.55")
+            ElseIf daypass = 14 Then
+                Console.WriteLine("14 Day Pass cost $239.95")
+            ElseIf daypass = 30 Then
+                Console.WriteLine("30 Day Pass cost $500.00")
+
+            End If
+            Call inputpass()
+        End While
     End Sub
 End Module
