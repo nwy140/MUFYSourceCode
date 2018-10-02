0a54d2ab3cdbe3fd8f0dbd7a9e6c6d90e4251e1d
NG WEI YUEN <17109182@SUNWAYEDU.LOCAL>
2018-10-02 09:45:19 +0800

    Quick com

--- vb.net/ScriptKiddieExercises/PseudocodeToApplication/Excercise 4/Excercise 4/Module1.vb
+++ vb.net/ScriptKiddieExercises/PseudocodeToApplication/Excercise 4/Excercise 4/Module1.vb
@@ -10,10 +10,12 @@
 
     End Sub
 
+    'User inputs guessnumber and has to enter guessnumber again if it is not within 1 to 100 and it is not the same as the guess number
     Sub inputnum()
         Console.WriteLine("Guess the magic number")
         guessnum = Console.ReadLine
         Call Evaluate()
+
         While magicnum <> guessnum Or guessnum < 1 Or guessnum > 100
             Console.WriteLine("Guess the magic number")
             guessnum = Console.ReadLine
@@ -21,12 +23,13 @@
         End While
     End Sub
 
+    'Checks if guess number is the same as magic number or it is lower or it is higher or it is out of range
     Sub Evaluate()
         If guessnum < 1 Or guessnum > 100 Then
             Console.WriteLine("Number out of range")
 
         ElseIf magicnum = guessnum Then
-        Console.WriteLine("You guess the magic number! " & magicnum)
+            Console.WriteLine("You guessed the magic number! " & magicnum)
         ElseIf magicnum > guessnum Then
         Console.WriteLine("Higher")
         ElseIf magicnum < guessnum Then
@@ -34,4 +37,5 @@
         End If
 
     End Sub
+
 End Module
--- vb.net/ScriptKiddieExercises/PseudocodeToApplication/Excercise 5/Excercise 5/Module1.vb
+++ vb.net/ScriptKiddieExercises/PseudocodeToApplication/Excercise 5/Excercise 5/Module1.vb
@@ -1,21 +1,20 @@
 ﻿Module Module1
-    ' Declare global variables
-    Dim i As Integer = 0
-    Dim Total As Integer = 0
-    Dim Average As Integer = 0
+
+    'Declare Global Variables 
+    Dim daypass As String
+    'Main procedure
     Sub Main()
-        Console.Write("Please enter number: ")
-        i = Console.ReadLine()
-        While i <> 999
-    
-            i = i + 1
-            Console.WriteLine("The number is " & i)
-            Total = Total + i
-            Console.WriteLine("The total is " & Total)
-            Average = Total / i
-            Console.WriteLine("The Average is " & Average)
-        End While
-        Console.WriteLine("The end")
+
+        Call inputpass()
+        Call evaluate()
     End Sub
 
+    Sub inputpass()
+        Console.WriteLine("Select 7 , 14 or 30 Day Pass, Enter Exit to exit")
+
+    End Sub
+
+    Sub evaluate()
+        While daypass <> "exit"
+    End Sub
 End Module
