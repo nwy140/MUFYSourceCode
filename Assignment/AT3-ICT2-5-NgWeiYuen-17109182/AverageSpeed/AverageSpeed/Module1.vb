fbe1f0e8c7a680fd290ea138baac9583d07308d0
Nwy <nwy140@gmail.com>
2018-10-24 16:38:56 +0800

    Finalize ... Black End Galaxy

---
+++ Assignment/AT3-ICT2-5-NgWeiYuen-17109182/AverageSpeed/AverageSpeed/Module1.vb
@@ -0,0 +1,101 @@
+﻿Module Module1
+
+    'Declare Global Variables
+    Dim vehreg As String
+    'Declare array speed
+    Dim speed(2) As Decimal
+    Dim averagespeed As Decimal
+    Dim fine As Boolean
+
+    'Main procedure to call functions and procedures
+    Sub Main()
+        'Call procedure to input vehicle registration 
+        Call inputvehreg()
+        'Call function that returns vehicle registration in Upper Case, then assign it to vehreg variable 
+        vehreg = upvehreg()
+
+        'Call procedure to input speed
+        Call inputspeed()
+        'Call getavgspeed function that returns Average speed based on speed array, then assign it to averagespeed variable 
+        averagespeed = getavgspeed()
+        'Call evaluate function that determines if vehicle gets a fine or not
+        fine = evaluate()
+
+        'Display Overall Information
+        display()
+    End Sub
+
+    '' Procedure where user inputs vehicle registration details. vehicle registration is validated to only accept 6 characters
+    Sub inputvehreg()
+        'User input vehicle registration 
+        Console.WriteLine("Please enter registration number")
+        vehreg = Console.ReadLine
+        'Validaties if vehicle registration has 6 characters, 
+        While Len(vehreg) <> 6
+            'display error message
+            Console.WriteLine("Invalid, input must be 6 characters, try again")
+            Console.WriteLine("Please enter registration number")
+            vehreg = Console.ReadLine
+        End While
+        ''/
+    End Sub
+
+    '' Function that converts vehicle registration to upper case and returns vehicle registration at upper case. Vehicle registration at upper case is returned to Main procedure
+    Function upvehreg()
+        Return UCase(vehreg)
+        ''/
+    End Function
+
+    '' Procedure where user input speed(1) and speed(2). Speed input is validated to only accept 0-400 numberic values.                  ''Km/h or mph?
+    Sub inputspeed()
+        For i = 1 To 2
+            'User input speed
+            Console.WriteLine("Please enter speed " & i)
+            speed(i) = Console.ReadLine
+            'Validaties if speed input is reasonable,  between 0 to 400 or user input is numeric
+            While speed(i) < 0 Or speed(i) > 400 Or IsNumeric(speed(i)) = False
+                'display error message
+                Console.WriteLine("Invalid, input must be between 0 to 400, try again")
+                Console.WriteLine("Please enter speed " & i)
+                speed(i) = Console.ReadLine
+            End While
+        Next
+    End Sub
+
+    '' Function that return averagespeed based on elements in speed array. averagespeed is returned to Main procedure
+    Function getavgspeed()
+        'Get Total of elements in speed array / 2
+        averagespeed = speed.Sum / 2
+        Return averagespeed
+    End Function
+
+    '' Function that is called to return true only when average>80. Function returns boolean to main procedure which is assigned to Fine variable
+    Function evaluate()
+        'Set boolean fine variable based on value of average speed
+        If (averagespeed > 80) Then
+            'Return as true if average > 80
+            fine = True
+            Return fine
+        Else
+            'Return as false otherwise
+            fine = False
+            Return fine
+        End If
+        'Return false again in case function has not returned anything
+        fine = False
+        Return fine
+    End Function
+
+    ''Procedure that display overall information based on what the user has input such as vehicle registration and averagespeed
+    Sub display()
+        'Display Overall Information
+        If fine = True Then
+            'Receive fine if fine is true
+            Console.WriteLine("Vehicle with registration number, " & vehreg & ", had an average speed of " & averagespeed & ". You will receive a fine.")
+        Else
+            'Will not Receive fine if fine is false
+            Console.WriteLine("Vehicle with registration number, " & vehreg & ", had an average speed of " & averagespeed & ". You will not receive a fine.")
+        End If
+    End Sub
+
+End Module
---
+++ Assignment/AT3-ICT2-5-NgWeiYuen-17109182/BuyingGroceries/BuyingGroceries/Module1.vb
@@ -0,0 +1,126 @@
+﻿Module Module1
+
+    'Declare Global Variables
+    Dim name As String
+    Dim itemsnum As Integer
+
+    'Declare price array with 5 elements
+    Dim price(5) As Decimal
+    Dim highest As Decimal
+    Dim total As Decimal
+    Dim discount As Decimal
+    Dim postdiscprice As Decimal
+
+    'Main procedure to call functions and procedures
+    Sub Main()
+        'Assign name as returned value from Function inputname
+        name = inputname()
+
+        'Call Procedure to input number of items bought
+        Call inputitemsnum()
+
+        'Call procedure to input price
+        Call inputprice()
+
+        'Assign highest as returned value from Function highprice
+        highest = highprice()
+
+        'Assign total as returned value from Function totalprice
+        total = totalprice()
+
+        'Assign discount as returned value from Function evaluate
+        discount = evaluate()
+
+        'Assign postdiscprice as returned value from Function calpostdisc
+        postdiscprice = calpostdisc()
+
+        'Display Overall Information
+        display()
+    End Sub
+
+    '' Function that returns user input for name. Name is returned to Main procedure.
+    Function inputname()
+        'User input name
+        Console.WriteLine("Please enter your name")
+        name = Console.ReadLine
+        Return name
+    End Function
+
+    '' Procedure where user input number of items bought. itemsnum is validated to only accept 0-5 in numeric values.
+    Sub inputitemsnum()
+        'User input number of items bought
+        Console.WriteLine("Please enter number of items bought")
+        itemsnum = Console.ReadLine
+        'Validaties if itemsnum input is reasonable,  itemsnum must be between 0 to 5 ,  user input must be numeric
+        While itemsnum < 0 Or itemsnum > 5 Or IsNumeric(itemsnum) = False
+            'display error message
+            Console.WriteLine("Invalid, input must be between 0 to 5, try again")
+            Console.WriteLine("Please enter number of items bought")
+            itemsnum = Console.ReadLine
+        End While
+
+    End Sub
+
+
+    '' Procedure where user input price for each items bought.price input for each items bought is validated to only accept positive numbers.
+    Sub inputprice()
+        'For loop iterates over number of items bought , and the user input the price for each of the items
+        For i = 1 To itemsnum
+            'User input price
+            Console.WriteLine("Please enter price for item " & i)
+            price(i) = Console.ReadLine
+            'Validaties if price input is numeric or price is positive
+            While price(i) < 0 Or IsNumeric(price(i)) = False
+                'display error message
+                Console.WriteLine("Invalid, input must be equal to or above 0, try again")
+                Console.WriteLine("Please enter price for item " & i)
+                price(i) = Console.ReadLine
+            End While
+        Next
+    End Sub
+
+    '' Function that return highestprice based on highest of the elements in price array. Highest price is returned to Main procedure
+    Function highprice()
+        highest = price.Max
+        Return highest
+    End Function
+
+    '' Function that return totalprice based on sum of the elements in price array. Total price is returned to Main procedure
+    Function totalprice()
+        total = price.Sum
+        Return total
+    End Function
+
+    '' Function that will return 5% discount of totalprice only when total price > 40. Discount is returned to Main procedure
+    Function evaluate()
+        ' 5% discount of totalprice if totalprice > 40
+        If total > 40 Then
+            discount = total * 0.05
+            Return discount
+            'Else no discount provided
+        Else
+            discount = 0
+            Return discount
+        End If
+    End Function
+    '' Function that return post-discount price . Post-discount price is returned to Main procedure
+    Function calpostdisc()
+        'subract total by discount
+        postdiscprice = total - discount
+        Return postdiscprice
+    End Function
+
+    ''Procedure that display overall information based of application including name, highest price, total price and discount informations
+    Sub display()
+        'Display Overall Information
+        If discount > 0 Then
+            'Qualify for discount if discount > 0 which means if there is discount
+            Console.WriteLine("Hello, " & name & ", the price of the most expensive item you bought is " & FormatCurrency(highprice()) & " and your total grocery bill is " & FormatCurrency(total) & ". You qualify for a discount of " & FormatCurrency(discount) & " and your post-discount bill is " & FormatCurrency(postdiscprice) & ".")
+        Else
+            'Else Will not Qualify for discount if there is no discount 
+            Console.WriteLine("Hello, " & name & ", the price of the most expensive item you bought is " & FormatCurrency(highprice()) & " and your total grocery bill is " & FormatCurrency(total) & ". You do not qualify for a discount.")
+
+        End If
+    End Sub
+
+End Module
